97a84de51166bc1987d4da9f2b6b11a38883599f
Georg Pietrek <georg.pietrek@conciso.de>
2022-10-19 16:53:17 +0200

    fix typo

--- kalkulator/daten.py
+++ kalkulator/daten.py
@@ -28,6 +28,6 @@ class Daten():
             description = wert.description()
             descriptions[key] = description
             print(f"    {key}: {description}")
-        data = {"values": values, "desriptions": descriptions}
+        data = {"values": values, "descriptions": descriptions}
         with open(filename, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
