8355a0e5473669bd11264f4b5e84f6139ef04bc7
Fabian-Kubiczek <116192650+Fabian-Kubiczek@users.noreply.github.com>
2022-11-03 21:23:12 +0100

    add data and main

---
+++ Alzheimer Classification/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+from skimage import data, io
+from matplotlib import pyplot as plt
+
+def load_images():
+
+    base_path = "./data"
+    img = io.imread(base_path + "/augmenteddata/MildDemented/0a0a0acd-8bd8-4b79-b724-cc5711e83bc7.jpg")
+    io.imshow(img)
+    io.show()
+
+    print(np.mean(img))
+
+
+if __name__ == '__main__':
+
+    print("Hello World")
+
+    load_images()
