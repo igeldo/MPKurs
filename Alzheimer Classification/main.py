e3c50abdabb5168f48162d3517930c4289d82462
inollmann <116682060+inollmann@users.noreply.github.com>
2022-11-06 17:13:21 +0100

    set paths

--- Alzheimer Classification/main.py
+++ Alzheimer Classification/main.py
@@ -1,17 +1,26 @@
 import numpy as np
 from skimage import data, io
 from matplotlib import pyplot as plt
-import tensorflow as tf
+#import tensorflow as tf
+import os
 
 def load_images():
 
-    base_path = "./data"
-    img = io.imread(base_path + "/augmenteddata/MildDemented/0a0a0acd-8bd8-4b79-b724-cc5711e83bc7.jpg")
-    io.imshow(img)
-    io.show()
+    #base_path = "./data"
+    #img = io.imread(base_path + "/augmenteddata/MildDemented/0a0a0acd-8bd8-4b79-b724-cc5711e83bc7.jpg")
+    #io.imshow(img)
+    #io.show()
 
-    print(np.mean(img))
-
+    #print(np.mean(img))
+    print(os.environ.get('USERNAME'))
+    if (os.environ.get('USERNAME') == 'kubic'):
+        path_alzheimer_folder = 'C:\\Users\\kubic\\Documents\\Alzheimer'
+    elif (os.environ.get('USERNAME') == 'ivono'):
+        if (os.path.exists('D:\FH\Master Dortmund\Programmierprojekt')):
+            path_alzheimer_folder = 'D:\\FH\\Master Dortmund\\Programmierprojekt\\Alzheimer'
+        else:
+            path_alzheimer_folder = 'C:\\Users\\ivono\\FH\\Programmierkurs\\Alzheimer'
+    print(path_alzheimer_folder)
 
 if __name__ == '__main__':
 
