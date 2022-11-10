8e5624975f86f910b40a6e7e531693a89516eb73
Fabian-Kubiczek <116192650+Fabian-Kubiczek@users.noreply.github.com>
2022-11-10 10:28:45 +0100

    added dummy class data

---
+++ Alzheimer Classification/Data.py
@@ -0,0 +1,31 @@
+class Data:
+
+    def __init__(self, traindata, testdata):
+
+        # in init schon einlesen?
+        self.traindata = traindata
+        self.testdata = testdata
+
+        self.images = []
+        self.database_access = []
+        self.layer = []
+
+
+    def load_images(self, images):
+
+        return images
+
+    def load_images_to_database(self, database_access):
+
+        # algorithm to upload data
+
+        return database_access
+
+    def get_images_from_database(self, database_access, layer):
+
+        # algorithm to
+        t = database_access
+
+        return t, layer
+
+
