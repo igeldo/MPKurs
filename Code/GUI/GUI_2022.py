ef53e9580f443adda8135c8b21081e6a3fb0c1cd
Oliver Jovanovic <oliver.jovanovic001@stud.fh-dortmund.de>
2022-11-15 10:26:58 +0100

    Started GUI.

--- Code/GUI/GUI_2022.py
+++ Code/GUI/GUI_2022.py
@@ -1,1 +1,24 @@
-# insert GUI code here
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("GUI for TI Radar Systems")
+
+        label = QLabel("This is a label")
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.setCentralWidget(label)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
