b8e3446efc4f43c757d3e6922baed2e75398fc9f
Oliver Jovanovic <oliver.jovanovic001@stud.fh-dortmund.de>
2022-11-16 12:38:24 +0100

    Improved GUI with functionality (Buttons and methods).

--- Code/GUI/GUI_2022.py
+++ Code/GUI/GUI_2022.py
@@ -1,24 +1,67 @@
-from PyQt6.QtGui import *
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
+# Importing necessary libraries
+import time
 
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QPushButton
+from PyQt6.QtCore import QSize, Qt
 import sys
 
-class MainWindow(QMainWindow):
+
+class Window(QMainWindow):
     def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
+        super(Window, self).__init__(*args, **kwargs)
 
+        # Setting up Window properties:
         self.setWindowTitle("GUI for TI Radar Systems")
+        self.setGeometry(100, 100, 1024, 768)
 
-        label = QLabel("This is a label")
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        # Calling method:
+        self.UiComponents()
 
-        self.setCentralWidget(label)
+        # Showing all widgets:
+        self.show()
+
+    def UiComponents(self):
+        # Creating connect button:
+        buttonConnect = QPushButton("Establish connection", self)
+        buttonConnect.setGeometry(200, 150, 150, 40)
+        buttonConnect.clicked.connect(self.clickmeConnect)
+
+        # Creating disconnect button:
+        buttonDisonnect = QPushButton("Disconnect", self)
+        buttonDisonnect.setGeometry(400, 150, 100, 40)
+        buttonDisonnect.clicked.connect(self.clickmeDisconnect)
+
+        buttonClose = QPushButton("Close Application", self)
+        buttonClose.setGeometry(550, 150, 100, 40)
+        buttonClose.clicked.connect(self.clickClose)
+
+    def clickmeConnect(self):
+        print("Please wait, connection is being established...")
+        time.sleep(0.5)
+        print("Connection established!")
+
+    def clickmeDisconnect(self):
+        print("Please wait, disconnection is being executed")
+        time.sleep(1)
+        print("Disconnection approved!")
+
+    def clickClose(self):
+        print("User pushed close button")
+        sys.exit(0)
+
+    def closeEvent(self, event):
+        print("User has clicked the close button on the main window!")
+        event.accept()
 
 
-app = QApplication(sys.argv)
 
-window = MainWindow()
-window.show()
+if __name__ == "__main__":
+    # Create PyQt6 App:
+    app = QApplication(sys.argv)
 
-app.exec()
+    # Create instance of the class Window:
+    window = Window()
+
+    # Starting the event loop / App:
+    sys.exit(app.exec())
