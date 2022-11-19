470c0547a36c1cff8e4cf4ca529820ac73dd32d3
lauragarpet <116182951+lauragarpet@users.noreply.github.com>
2022-11-19 14:58:07 +0100

    Delete trash.py

--- Applied programming project/Project/trash.py
+++
@@ -1,5 +0,0 @@
-features_list = featureScores["Specs"].tolist()[:10]
-# feature_list in data frame übergeben und nur die entsprechenden Features behalten --> drop
-self.df = self.df[
-    ['sysBP', 'glucose', 'age', 'totChol', 'cigsPerDay', 'diaBP', 'prevalentHyp', 'diabetes', 'BPMeds', 'male',
-     'TenYearCHD']]
