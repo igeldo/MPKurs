19b4f536a042a2a954b7949630b797f14215cb3b
jroem <julian.roemer004@stud.fh-dortmund.de>
2022-12-21 15:49:08 +0100

    init plotIO.py

--- plotIO.py
+++ plotIO.py
@@ -6,4 +6,4 @@ def output(path, filename, delimiter=';'):
     file = open(os.path.join(path, filename), "w", newline='')
     writer = csv.writer(file, delimiter=delimiter)
     writer.writerow(['x', 'y', 'z', 'ux', 'uy', 'uz', 'stepSizeL'])
-    writer.writerow([x,y,z,ux,uy,uz,stepSizeL]) #  TODO: mäke gedänken drüber
+    writer.writerow([x,y,z,ux,uy,uz,stepSizeL]) # TODO: mäke gedänken drüber
