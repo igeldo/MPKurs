447f42fd0a30257371c371bdf6253b0a92b2fea2
jroem <julian.roemer004@stud.fh-dortmund.de>
2022-12-21 18:21:04 +0100

    class material for heritage to glass and tissue

--- run.py
+++ run.py
@@ -1,27 +1,33 @@
-class StepSize:
+class Material:
     def __init__(self):
-        self.inGlass()
-        self.inTissue()
-
-    def inGlass(self, photonPack, layer):
-        if(photonPack.__dvec(3) > 0.0):
-            stepsToBoundry = (layer.z1)
-
-class Hop:
-    def __init__(self):
-        self.__hop()
-        self.inGlass()
-        self.inTissue()
 
     def __hop(self, photonPack):
         photonPack.__pos += photonPack.__dvec * photonPack.__stepSize
         #self.__y += self.stepSize * self.__uy
         #self.__z += self.stepSize * self.__uz
 
+
+class Glas(Material):
+    def __init__(self):
+        super.__init__()
+
+    def hop(self):
+    ''''
     def inGlass(self, photonPack):
-        if(photonPack.__dvec(3) == 0): # 3rd dimension is uz
+        if (photonPack.__dvec(3) == 0):  # 3rd dimension is uz
             photonPack.__dead = 1
         else:
             StepSize.inGlass()
             self.hop(photonPack)
+    '''
 
+    def stepSize(self, photonPack, layer):
+        if(photonPack.__dvec(3) > 0.0):
+            stepsToBoundry = (layer.z1)
+
+
+class Tissue(Material):
+    def __init__(self):
+        super.__init__()
+
+    def stepSize(self):
