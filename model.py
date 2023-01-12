1abcee3ee3bcff4504cc4542b7c92396af7328e5
jroem <julian.roemer004@stud.fh-dortmund.de>
2023-01-12 15:36:50 +0100

    included abstract class Medium, changed __repr__ for PhotonPack - not final

--- model.py
+++ model.py
@@ -34,13 +34,14 @@ class PhotonPack(Vec3d):
 
     def __init__(self, pos=Vec3d(0,0,0), layer=1, stepSize=0, stepSizeL=0,
                  dvec=Vec3d(0,0,1), w=1, dead=0):
-        self.__pos = pos  # coordinates [mm]
-        self.__dvec = dvec  # directional cosines of photonpack
-        self.__w = w  # "weight", more like energy?
-        self.__dead = dead  # 1 if photon is "terminated"(absorpted or reflected)
-        self.__layer = layer  # layer in with the PhotonPack currently is
-        self.__stepSize = stepSize  # current step size [mm]
-        self.__stepSizeL = stepSizeL  # step size left, dimensionless, because it's relative to layer material stepSizeL = ()
+        self._pos = pos  # coordinates [mm]
+        self._dvec = dvec  # directional cosines of photonpack
+        self._w = w  # "weight", more like energy?
+        self._dead = dead  # 1 if photon is "terminated"(absorpted or reflected)
+        self._layer = layer  # layer in with the PhotonPack currently is
+        # stepSize is handled and calculated in each layer based on its properties and the photon energy/weight
+        self._stepSize = stepSize  # current step size [mm]
+        #self.__stepSizeL = stepSizeL  # step size left, dimensionless, because it's relative to layer material stepSizeL = ()
 
     def __repr__(self):
         """
@@ -57,11 +58,21 @@ class PhotonPack(Vec3d):
             layer :
                 Index of the layer where the PhotonPack resides
         """
-        return "{}, {}, {}, {}, {}".format(
-                self.__pos, self.__dvec, self.__layer, self.__w, self.__dead)
+        #return "{}, {}, {}, {}, {}".format(
+         #       self._pos, self._dvec, self._layer, self._w, self._dead)
+        return [
+            str(self._pos.x()),
+            str(self._pos.y()),
+            str(self._pos.z()),
+            str(self._dvec.x()),
+            str(self._dvec.y()),
+            str(self._dvec.z()),
+            str(self._layer),
+            str(self._w),
+            str(self._dead)
+        ]
 
-
-class Layer:
+class Medium:
     """
     Basic class to simulate a layer.
     
@@ -74,7 +85,7 @@ class Layer:
     n : 
         refractive index of the layer
     mua, mus : 
-        absorption and scattering coefficient in [1/mm]
+        absorption and scattering coefficient in [1/mm] TODO: typischerweise in cm -> umrechnen *1/10
     g : 
         anisotropy of the material
     cos_crit0, cos_crit1 : 
@@ -84,20 +95,60 @@ class Layer:
     -------
     """
 
-    def __init__(self, z0, z1, mua, mus, g, n=1):
+    def __init__(self, z0, z1, mua, mus, g, cos_crit0, cos_crit1, n=1):
         self.z0, self.z1 = z0, z1  # z coordinates of the upper (z0) and lower (z1) boundary of the respective layer [mm]
         self.n = n  # refractive index (Brechungsindex) of the respective layer
         self.mua, self.mus = mua, mus  # absorption and scattering coefficient [1/mm]
         self.g = g  # anisotropy of the layer material
-        self.cos_crit0, self.cos_crit1  # critical angles under which total reflection occurs(?) so there is nothing to compute in this layer
+        self.cos_crit0, self.cos_crit1 = cos_crit0, cos_crit1  # critical angles under which total reflection occurs(?) so there is nothing to compute in this layer
 
-class Medium:
-    def __init__(self, layer1, layer2, layer3): # zukünftig mit 'args?
-        # for l in len(args):
-        #   layer_l = args(l)
-        self.layer1 = layer1
-        self.layer2 = layer2
-        self.layer3 = layer3
+    def _hop(self, photonPack):
+        s = -np.log(np.random.uniform())/(self.mua+self.mus)
+        #print(s)
+        photonPack._pos += photonPack._dvec * s
 
-        # fehlt hier noch eigentschaften gedönns? z.B. dz,dr,da
-        # und ist grid üeberhaupt nötig? -> mit vincent mal besprechen
+class Glas(Medium):
+
+
+    #def hop(self):
+        # """
+        # def inGlass(self, photonPack):
+        #     if (photonPack.__dvec.z == 0):  # 3rd dimension is uz
+        #         photonPack.__dead = 1
+        #     else:
+        #         StepSize.inGlass()
+        #         self.hop(photonPack)
+        # """
+
+    def stepSize(self, photonPack, layer):
+        if(photonPack._dvec.z > 0.0): # TODO z falsch?
+            stepsToBoundry = (layer.z1)
+
+
+class Tissue(Medium):
+
+    def __int__(self):
+        super().__init__() #TODO: siehe TODO hop
+
+    def _hop(self, photonPack):
+        super()._hop(photonPack) # TODO: Warum hier mit Argument, aber nicht bei __init()__? @vincent @alex
+
+    #def stepSize(self):
+        """
+        p.14 & p.19 eq. 2.7 "total interaction coefficient µt, which is the sum of the
+absorption coefficient µa and the scattering coefficient µs"
+        Returns
+        -------
+
+        """
+
+    def _absorption(self, photonPack):
+        dw = photonPack._w * self.mua / (self.mua + self.mus)
+        photonPack._w = dw
+
+
+# class Luft(Medium):
+#     def __int__(self):
+#         super.__init__()
+#
+#     def stepSize(self):
