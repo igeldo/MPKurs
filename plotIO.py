534b8b5ebd611aa68e77b46157ae0bcdc31d8c6d
jroem <julian.roemer004@stud.fh-dortmund.de>
2023-01-12 15:37:19 +0100

    wrote a first plot routine for straight flying photon with absorption

--- plotIO.py
+++ plotIO.py
@@ -1,9 +1,22 @@
 import os
 import csv
+
+import pandas as pd
+
 import matplotlib.pyplot as plt
 
-def output(path, filename, delimiter=';'):
-    file = open(os.path.join(path, filename), "w", newline='')
-    writer = csv.writer(file, delimiter=delimiter)
-    writer.writerow(['x', 'y', 'z', 'ux', 'uy', 'uz', 'stepSizeL'])
-    writer.writerow([x,y,z,ux,uy,uz,stepSizeL]) # TODO: mäke gedänken drüber
+
+if __name__ == '__main__':
+
+    OUTPATH = 'out'
+    FILENAME = 'test.csv'
+
+    df = pd.read_csv(
+        os.path.join(OUTPATH, FILENAME), sep=';', header=0
+    )
+    print(df['z'].head())
+    fig, ax = plt.subplots(dpi=200)
+    df.plot.scatter(x = 'y', y = 'z', s=0.1, c = 'weight', colormap='viridis', ax=ax)
+    plt.xlabel('y [mm]')
+    plt.ylabel('z [mm]')
+
