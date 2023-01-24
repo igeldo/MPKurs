ab0119c03b44ce0bc31247aed5c7bf6139ed27da
Oleminator1 <79644589+Oleminator1@users.noreply.github.com>
2023-01-24 17:24:53 +0100

    Cleaned the repository.

--- Code/GUI/pymmwave_GUI_MVC.py
+++ Code/GUI/pymmwave_GUI_MVC.py
@@ -1,162 +1,183 @@
-# Import necessary modules
+# Import necessary modules:
+import os
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QLabel,
-                             QComboBox, QPushButton, QGridLayout, QMessageBox, QAction)
-from PyQt5.QtCore import Qt
+                             QComboBox, QPushButton, QVBoxLayout, QMessageBox)
 from PyQt5.QtGui import (QFont, QIcon)
 from serial.tools import list_ports
 
-# TODO: Import following functionality from pymmwave_GUI.py: aboutDialog(), update_ports(), connect_radar()
-
-
-class MainWindow(QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-        self.initializeUI()
-        self.check_ports()
-
-    def initializeUI(self):
-        """Set up the application's GUI."""
-        self.setMinimumSize(300, 150)
-        self.setWindowTitle("GUI for PymmWave")
-        self.setWindowIcon(QIcon("images/pyqt_logo.png"))
-
-        # Create model
-        self.model = Model()
-
-        # Create view
-        self.view = View(self.model)
-        self.setCentralWidget(self.view)
-
-        # Create controller
-        self.controller = Controller(self.model, self.view)
-
-        self.createActions()
-        self.createMenu()
-        self.show()
-
-    def createActions(self):
-        """Create the application's menu actions."""
-        # Create the actions for File menu
-        self.quit_act = QAction(QIcon("images/exit.png"), "Quit")
-        self.quit_act.setShortcut("Ctrl+Q")
-        self.quit_act.triggered.connect(self.close)
-
-    def createMenu(self):
-        """Create the application's menu bar."""
-        self.file_menu = self.menuBar().addMenu("File")
-        self.file_menu.addAction(self.quit_act)
-
-    def check_ports(self):
-        """Check if the selected ports are the same."""
-        if self.model.tx_com_port == self.model.rx_com_port:
-            QMessageBox.warning(self, "Warning", "TX and RX ports cannot be the same.")
-            self.controller.disable_connect_button()
-
-    def closeEvent(self, event):
-        """Prompt the user to confirm that they want to close the application."""
-        result = QMessageBox.question(self, "Confirm Exit", "Are you sure you want to exit?", QMessageBox.Yes |
-                                      QMessageBox.No)
-        event.accept() if result == QMessageBox.Yes else event.ignore()
-
 
 class Model:
+    """With which data do we want to work?"""
     def __init__(self):
+        self.tx_port = None
+        self.rx_port = None
         self.ports = [port.device for port in list_ports.comports()]
-        self.tx_com_port = None
-        self.rx_com_port = None
 
 
-class View(QWidget):
-    def __init__(self, model):
-        super().__init__()
-        self.model = model
-        self.initializeUI()
+class View(QMainWindow):
+    """How should the GUI look like?"""
+    def __init__(self, model, controller):
+        super(View, self).__init__()
+        self._model = model
+        self.tx_port = None
+        self.rx_port = None
+        self.initUI(controller)
+        self.quit_button.clicked.connect(controller.close)
+        self.button.clicked.connect(controller.buttonClicked)
 
-    def initializeUI(self):
-        """Create and arrange widgets in the main window."""
+    def initUI(self, controller):
+        layout = QVBoxLayout()
+        self.setMinimumSize(300, 150)
+        self.setWindowTitle("PymmWave Execution GUI")
+        self.setWindowIcon(QIcon("images/pyqt_logo.png"))
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
         # Headline
         header_label = QLabel("Properties")
-        header_label.setFont(QFont("Helvetica", 14))
-        header_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        header_label.setFont(QFont("Helvetica", 12))
 
-        # Select Ports
-        tx_com_port_label = QLabel("TX COM Port:", self)
-        tx_com_port_label.setFont(QFont("Helvetica", 12))
-        self.tx_com_port = QComboBox()
-        self.tx_com_port.addItems(self.model.ports)
-        self.tx_com_port.activated.connect(self.txPortsChoose)
+        # Ports
+        # tx port
+        tx_port_label = QLabel("Select te TX port:", self)
+        tx_port_label.setFont(QFont("Helvetica", 12))
+        self.tx_port = QComboBox()
+        self.tx_port.addItems(controller._model.ports)
 
-        rx_com_port_label = QLabel("RX COM Port:", self)
-        rx_com_port_label.setFont(QFont("Helvetica", 12))
-        self.rx_com_port = QComboBox()
-        self.rx_com_port.addItems(self.model.ports)
-        self.rx_com_port.activated.connect(self.txPortsChoose)
+        # rx port
+        rx_port_label = QLabel("Select the RX port:", self)
+        rx_port_label.setFont(QFont("Helvetica", 12))
+        self.rx_port = QComboBox()
+        self.rx_port.addItems(controller._model.ports)
 
         # Connect and Disconnect Button in one
         self.times_pressed = 0
-        self.button = QPushButton("Connect Radar")
+        self.button = QPushButton("Connect")
         self.button.setFont(QFont("Helvetica", 12))
 
-        # Organize the left side widgets into column 0 of the QGridLayout
-        main_grid = QGridLayout()
-        main_grid.addWidget(header_label, 0, 0)
-        main_grid.addWidget(tx_com_port_label, 1, 0)
-        main_grid.addWidget(self.tx_com_port, 1, 1)
-        main_grid.addWidget(rx_com_port_label, 2, 0)
-        main_grid.addWidget(self.rx_com_port, 2, 1)
-        main_grid.addWidget(self.button, 4, 0)
+        # Quit Button
+        self.quit_button = QPushButton("Quit")
+        self.quit_button.setFont(QFont("Helvetica", 12))
 
-        # Set the layout for the main window
-        self.setLayout(main_grid)
+        # Add widgets to layout
+        layout.addWidget(header_label)
+        layout.addWidget(tx_port_label)
+        layout.addWidget(self.tx_port)
+        layout.addWidget(rx_port_label)
+        layout.addWidget(self.rx_port)
+        layout.addWidget(self.button)
+        layout.addWidget(self.quit_button)
 
-    def txPortsChoose(self):
-        """Save the selected TX COM port."""
-        self.model.tx_com_port = self.tx_com_port.currentText()
+        # Create menu bar
+        menu_bar = self.menuBar()
 
-    def rxPortsChoose(self):
-        """Save the selected RX COM port."""
-        self.model.rx_com_port = self.rx_com_port.currentText()
+        # Create File menu
+        file_menu = menu_bar.addMenu("File")
+        refresh_action = file_menu.addAction("Refresh", controller.update_ports)
+        icon_refresh = QIcon("images/refresh.png")
+        refresh_action.setIcon(icon_refresh)
+        file_menu.addSeparator()
+        exit_action = file_menu.addAction("Exit", controller.close)
+        icon_exit = QIcon("images/exit.png")
+        exit_action.setIcon(icon_exit)
+
+        # Create Help menu
+        help_menu = menu_bar.addMenu("Help")
+        about_action = help_menu.addAction("About", controller.about)
+        icon_about = QIcon("images/about.svg")
+        about_action.setIcon(icon_about)
+
+        self.tx_port.currentTextChanged.connect(controller.check_ports)
+        self.rx_port.currentTextChanged.connect(controller.check_ports)
 
 
 class Controller:
-    def __init__(self, model, view):
-        self.model = model
-        self.view = view
-        self.view.button.clicked.connect(self.buttonClicked)
-        self.view.tx_com_port.activated.connect(self.txPortsChoose)
-        self.view.rx_com_port.activated.connect(self.rxPortsChoose)
-
-    def buttonClicked(self):
-        """Handle the button clicked event."""
-        self.times_pressed += 1
-        if self.times_pressed % 2 != 0:
-
-            self.view.button.setText("Disconnect Radar")
-            # Perform connect logic here
-        else:
-            self.view.button.setText("Connect Radar")
-            # Perform disconnect logic here
-
-    def disable_connect_button(self):
-        """Disable the connect button."""
-        self.view.button.setEnabled(False)
-
-    def txPortsChoose(self):
-        """Save the selected TX COM port."""
-        self.model.tx_com_port = self.view.tx_com_port.currentText()
-
-    def rxPortsChoose(self):
-        """Save the selected RX COM port."""
-        self.model.rx_com_port = self.view.rx_com_port.currentText()
+    """How should the GUI behave?"""
+    def __init__(self):
+        self._app = QApplication(sys.argv)
+        self._model = Model()
+        self._view = View(self._model, self)
+        self._view.button.clicked.connect(self.buttonClicked)
+        self.check_ports()
 
     def close(self):
-        """Close the application"""
-        self.view.close()
+        """Prompt the user to confirm that they want to close the application."""
+        result = QMessageBox.question(self._view, "Confirm Exit", "Are you sure you want to exit?", QMessageBox.Yes |
+                                      QMessageBox.No, QMessageBox.No)
+        if result == QMessageBox.Yes:
+            self._app.quit()
+
+    def connect(self):
+        """Connect the radar"""
+        tx_port = self._view.tx_port.currentText()
+        rx_port = self._view.rx_port.currentText()
+        self._model.tx_port = tx_port # Should be COM3
+        self._model.rx_port = rx_port # Should be COM4
+        print(f'Connected to {tx_port} and {rx_port}')
+        os.chdir(r'C:\Users\Olive\PycharmProjects\MPKurs\Code\pymmw-master\source')
+        os.system('python pymmw.py -c ' + rx_port + ' -d ' + tx_port)
+        print("pymmwave started.")
+        print("python pymmw.py -c {tx_port} -d {rx_port}")
+
+    def disconnect(self):
+        print("Disconnected.")
+
+    def check_ports(self):
+        """Check if ports are not empty, if they are empty make them not choosable."""
+        if not self._model.ports:
+            self._view.tx_port.setEnabled(False)
+            self._view.rx_port.setEnabled(False)
+            self._view.button.setEnabled(False)
+            print("No ports available.")
+        else:
+            self._view.tx_port.setEnabled(True)
+            self._view.rx_port.setEnabled(True)
+            self._view.button.setEnabled(True)
+            print("Ports available.")
+            tx_port = self._view.tx_port.currentText()
+            rx_port = self._view.rx_port.currentText()
+            """Check if ports are the same, if true, then disable the connect button!"""
+            if tx_port == rx_port:
+                self._view.button.setEnabled(False)
+                print("Ports are the same.")
+            else:
+                self._view.button.setEnabled(True)
+                print("Ports are not the same.")
+
+    def update_ports(self):
+        """Updates the list of available ports in the Model class and updates the options in the
+        QComboBox widgets of the View class accordingly."""
+        self._model.ports = [port.device for port in list_ports.comports()]
+        self._view.tx_port.clear()
+        self._view.tx_port.addItems(self._model.ports)
+        self._view.rx_port.clear()
+        self._view.rx_port.addItems(self._model.ports)
+        self.check_ports()
+        print("Ports updated!")
+
+    def buttonClicked(self):
+        """If button_clicked is uneven, then show "Connect", otherwise disconnect"""
+        if self._view.times_pressed % 2 != 0:
+            self.disconnect()
+            self.button.setText("Connect")
+        else:
+            self.connect()
+            self._view.button.setText("Disconnect")
+            self._view.times_pressed += 1
+
+    def about(self):
+        """Show an about-dialog."""
+        QMessageBox.about(self._view, "About", """<p>This GUI should help you control TI Radars</p>
+        <p>Created by Oliver Jovanović</p>""")
+
+    def run(self):
+        self._view.show()
+        sys.exit(self._app.exec_())
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    sys.exit(app.exec_())
+    c = Controller()
+    sys.exit(c.run())
