e0c3f50e340e5dd92e4439a794e21ec80c014612
jroem <julian.roemer004@stud.fh-dortmund.de>
2023-02-14 15:40:10 +0100

    included roulette

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import os
 import csv
-from model import Tissue, PhotonPack
+from model import Tissue, PhotonPack, WEIGHT
 
 if __name__ == '__main__':
 
     # define iofiles
     OUTPATH = 'out'
-    FILENAME = 'test.csv'
+    FILENAME = 'test2.csv'
 
     # define params
-    photon1 = PhotonPack(stepSize=1, w=100)
-    layer1 = Tissue(z0=0, z1=1000, mua=1, mus=100, g=0.9, cos_crit0=0,cos_crit1=0)
+    photon1 = PhotonPack(stepSize=1, w=1)
+    layer1 = Tissue(z0=0, z1=50, mua=1, mus=100, g=0.9, cos_crit0=0, cos_crit1=0)
+    layer2 = Tissue(z0=0, z1=50, mua=1, mus=100, g=0.9, cos_crit0=0, cos_crit1=0)
 
-    layers = list(layer1)
-    photons = list(photon1)
+    layers = [layer1]
+    photons = [photon1]
 
     # create output
     if not os.path.exists(os.path.join(OUTPATH)):
@@ -23,18 +24,21 @@ if __name__ == '__main__':
     writer.writerow(['x', 'y', 'z', 'ux', 'uy', 'uz', 'layer', 'weight', 'dead'])
 
     # start sim
-    print(photon1.__repr__()) # TODO: check repr
+    #print(p.__repr__()) # TODO: check repr
 
     for p in photons:
-        if p.alive == 0:
-            while(not layers[p._layer].hitBoundry):
-                        layers[p._layer].hop(p)
-                        layers[p._layer].absorption(p)
-                        layers[p._layer].scatter(p)
-                        writer.writerow(p.__rep)
+        while not layers[p._layer].hitBoundry(p) and p.alive() == 0:
+            layers[p._layer].hop(p)
+            layers[p._layer].absorption(p)
+            layers[p._layer].scatter(p)
+            writer.writerow(p.__repr__())
+            if p._w < WEIGHT and p.alive() == 0:
+                p.roulette()
+
+        if p.alive() == 0:
             layers[p._layer].hop(p)
             layers[p._layer].crossOrNot(p, layers)
-
+            writer.writerow(p.__repr__())
 
     # for i in range(0, 100):
     #     layer1.hop(photon1)
