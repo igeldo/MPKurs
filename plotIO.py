39c3bf8f59c5e4d4f48a002239c2144eed1e53cd
jroem <julian.roemer004@stud.fh-dortmund.de>
2023-04-25 10:50:49 +0200

    adapted quiver for own directions, inverted y-axis for better readability (positive values downwards), adjusted plot for better readability

--- plotIO.py
+++ plotIO.py
@@ -21,24 +21,38 @@ if __name__ == '__main__':
     
     x = df['x'].values
     y = df['y'].values
-    u = df['ux'].values
-    v = df['uy'].values
+    z = df['z'].values
     w = df['weight'].values
-    
-    u = np.diff(x)
-    v = np.diff(y)
-    pos_x = x[:-1] + u/2
-    pos_y = y[:-1] + v/2
-    norm = np.sqrt(u**2+v**2) 
-    
-    fig, ax = plt.subplots()
-    ax.plot(x,y, marker="o")
-    ax.quiver(pos_x, pos_y, u/norm, v/norm, angles="xy", zorder=5, pivot="mid")
-    plt.show()
-    
-    #print(df['z'].head())
+
+    # u = np.diff(x)
+    # v = np.diff(y)
+    # pos_x = x[:-1] + u/2
+    # pos_y = y[:-1] + v/2
+    # norm = np.sqrt(u**2+v**2)
+    #
+    # fig, ax = plt.subplots(dpi=200)
+    # ax.scatter(x,y, marker="o", s=0.1)
+    # ax.quiver(pos_x, pos_y, u/norm, v/norm, angles="xy", zorder=5, pivot="mid")
+    # plt.xlabel("x [mm]")
+    # plt.ylabel("y [mm]")
+    # plt.show()
+
+    ux = df['ux'].values
+    uy = df['uy'].values
+    uz = df['uz'].values
+
+    # fig, ax = plt.subplots(dpi=200)
+    # ax.scatter(x,z, marker="o", s=0.1)
+    # ax.quiver(x, z, ux, uz, angles="xy", zorder=5, pivot="mid")
+    # plt.gca().invert_yaxis()
+    # plt.xlabel("x [mm]")
+    # plt.ylabel("z [mm]")
+    # plt.show()
+
     fig, ax = plt.subplots(dpi=200)
-    df.plot.scatter(x = 'x', y = 'z', s=0.1, c = 'weight', colormap='viridis', ax=ax)
+    df.plot.scatter(x = 'x', y = 'z', s=2, c = 'weight', colormap='viridis', ax=ax)
+    ax.quiver(x, z, ux, uz, angles="xy", pivot="mid", color='black', alpha=0.3)
+    plt.gca().invert_yaxis()
     plt.xlabel('x [mm]')
     plt.ylabel('z [mm]')
     if SAVE:
